e669e5c0012cb7ed764b132f7d2333b1f7f99f44
Default Seadas User <seadas-user@localhost>
2018-03-30 08:19:26 -0400

    still won't run properly

--- modules/imagery/anomaly_difference.R
+++ modules/imagery/anomaly_difference.R
@@ -6,68 +6,87 @@ library(maps)
 library(mapdata)
 library(maptools)
 library(grid)
+library(png)
+library(magick)
 
- args     <- commandArgs(TRUE)
- lastline <- args[1]
-  #lastline <- "/aqua.2018003.0103-2018010.0110.D.L3.modis.NAT.v09.1000m.nc4"
-    for (j in seq(-15,15,1)){
-    llyr       <- as.numeric(substr(lastline, 7, 10))
-    lljday     <- as.numeric(substr(lastline, 11, 13)) + j
-    
-    if (lljday < 1){
-      lljday <- ifelse(leap_year(llyr), 366 + lljday, 365 + lljday)
-      llyr   <- llyr - 1
-    }
-    # Figure out Day Of Interest
-    doi <- as.Date(as.numeric(lljday) - 1, origin = paste0(llyr, "-01-01"))
-    
-    # We have to do this or else it'll go back through all the data
-    if (doi < as.Date('2018-01-01')){
-      llyr   <- 2018
-      lljday <- 1
-      doi    <- as.Date(as.numeric(lljday), origin = paste0(llyr, "-01-01"))
-    }
-    days.in.year = ifelse(leap_year(llyr), 366, 365)
-    
-    ejday = as.numeric(lljday)
-    eyr = year(doi)
-    emn = month(as.Date(ejday, origin = paste0(eyr, "-01-01")))
-    edom = day(as.Date(ejday, origin = paste0(eyr, "-01-01"))) - 1
+copyright.draw <- function(label, image, x, y, size, ...) {
+  lab <- textGrob(label = label,
+                  x = unit(x, "npc"), y = unit(y, "npc"),
+                  just = c("left", "centre"), gp = gpar(...))
+  logo <- rasterGrob(image = image,
+                     x = unit(x, "npc") + unit(1, "grobwidth", lab), y = unit(y, "npc"),
+                     width = unit(size, "cm"), height = unit(size, "cm"),
+                     just = c("left", "centre"), gp = gpar(...))
+  grid.draw(lab)
+  grid.draw(logo)
+}
+im1 = readPNG("/home/sat_ops/goes_r/nexrad/cema38.png")
+im2 = readPNG("/home/sat_ops/goes_r/nexrad/udel38.png")
 
-    jday = ifelse(ejday + 7 > days.in.year, ejday + 7 - days.in.year, ejday + 7)
-    yr = ifelse(jday < 7, year(doi) + 1, year(doi))
-    mn = month(as.Date(jday, origin = paste0(yr, "-01-01")))
-    dom = day(as.Date(jday, origin = paste0(yr, "-01-01"))) - 1
 
-    
-    # add 0s where needed to match the filename string
-    ejday = ifelse(ejday > 99, ejday, ifelse(ejday > 9, paste0("0", ejday), paste0("00", ejday)))
-    edom = ifelse(edom > 9, edom, paste0("0", edom))
-    emn = ifelse(emn > 9, emn, paste0("0", emn))
-    jday = ifelse(jday < 10, paste0("00",jday), ifelse(jday < 100, paste0("0",jday), jday))
-    dom = ifelse(dom > 9, dom, paste0("0",dom)) #dom = day of month
-    mn = ifelse(mn > 9, mn, paste0("0",mn))
-    
-    
-    #fname represents the day of interest
-    fname = paste0("/aqua.",eyr, ejday, ".", emn, edom,
-                   "-", yr, jday, ".", mn, dom, ".D.L3.modis.NAT.v09.1000m.nc4")
-    
-    # now here is the full file
-    aqua.file = paste0("/data/Aqua/8_day/",eyr,fname)
-    if (file.exists(aqua.file) == TRUE){
-      # if the file hasn't been processed, place it in logfile so we don't run it again next hour
-      if (any(grep(fname, readLines("/home/james/anomalies/logfile.txt"))) == FALSE){
-        cat(paste0(fname,"\n"), file = "/home/james/anomalies/logfile.txt", append = TRUE)
-        break #since the file exists and we haven't run it yet, we need to exit the loop
-      }
-    }
-    
-    # if no file exists, then we better stop the whole operation because it'll crash anyway
-    if (j == 15 && file.exists(aqua.file) == FALSE){
-      stop("No Files Exists")
+args     <- commandArgs(TRUE)
+lastline <- args[1]
+
+# this loop goes through the available files in the Aqua data folder and compares what hasn't been done 
+# in the logfile.txt. We go through the last 15 files and the next 15 files because sometimes we miss a 
+# day because processing errors take place
+for (j in seq(-15,15,1)){
+  llyr       <- as.numeric(substr(lastline, 7, 10))
+  lljday     <- as.numeric(substr(lastline, 11, 13)) + j
+  if (lljday < 1){
+    lljday <- ifelse(leap_year(llyr), 366 + lljday, 365 + lljday)
+    llyr   <- llyr - 1
+  }
+  # Figure out Day Of Interest
+  doi <- as.Date(as.numeric(lljday) - 1, origin = paste0(llyr, "-01-01"))
+  
+  # We have to do this or else it'll go back through all the data
+  if (doi < as.Date('2018-01-01')){
+    llyr   <- 2018
+    lljday <- 1
+    doi    <- as.Date(as.numeric(lljday), origin = paste0(llyr, "-01-01"))
+  }
+  days.in.year = ifelse(leap_year(llyr), 366, 365)
+  
+  ejday = as.numeric(lljday)
+  eyr = year(doi)
+  emn = month(as.Date(ejday, origin = paste0(eyr, "-01-01")))
+  edom = day(as.Date(ejday, origin = paste0(eyr, "-01-01"))) - 1
+
+  jday = ifelse(ejday + 7 > days.in.year, ejday + 7 - days.in.year, ejday + 7)
+  yr = ifelse(jday < 7, year(doi) + 1, year(doi))
+  mn = month(as.Date(jday, origin = paste0(yr, "-01-01")))
+  dom = day(as.Date(jday, origin = paste0(yr, "-01-01"))) - 1
+
+  
+  # add 0s where needed to match the filename string
+  ejday = ifelse(ejday > 99, ejday, ifelse(ejday > 9, paste0("0", ejday), paste0("00", ejday)))
+  edom = ifelse(edom > 9, edom, paste0("0", edom))
+  emn = ifelse(emn > 9, emn, paste0("0", emn))
+  jday = ifelse(jday < 10, paste0("00",jday), ifelse(jday < 100, paste0("0",jday), jday))
+  dom = ifelse(dom > 9, dom, paste0("0",dom)) #dom = day of month
+  mn = ifelse(mn > 9, mn, paste0("0",mn))
+  
+  
+  #fname represents the day of interest
+  fname = paste0("/aqua.",eyr, ejday, ".", emn, edom,
+                 "-", yr, jday, ".", mn, dom, ".D.L3.modis.NAT.v09.1000m.nc4")
+  
+  # now here is the full file
+  aqua.file = paste0("/data/Aqua/8_day/",eyr,fname)
+  if (file.exists(aqua.file) == TRUE){
+    # if the file hasn't been processed, place it in logfile so we don't run it again next hour
+    if (any(grep(fname, readLines("/home/james/anomalies/logfile.txt"))) == FALSE){
+      cat(paste0(fname,"\n"), file = "/home/james/anomalies/logfile.txt", append = TRUE)
+      break #since the file exists and we haven't run it yet, we need to exit the loop
     }
   }
+  
+  # if no file exists, then we better stop the whole operation because it'll crash anyway
+  if (j == 15 && file.exists(aqua.file) == FALSE){
+    stop("No Files Exists")
+  }
+}
 
   
   # read in the aqua file
@@ -89,11 +108,15 @@ library(grid)
   aqua.raster <- rasterize(aqua.xyv[, 1:2], aqua.raster, aqua.xyv[,3], fun=mean) 
   
   # read in the specific 8 day segment of the 8 day climatology
-  clim.nc = nc_open("/data/Aqua/climatology/8_day/8_day_climatology.nc4")
-  e.day <- ceiling(((as.numeric(jday) + as.numeric(ejday)) / 2)/8)
+  clim.nc = nc_open("http://basin.ceoe.udel.edu/thredds/dodsC/aqua_clim_rolling8.nc")
+  rec_time  <- max(clim.nc$dim$time$vals)
+  rec_len   <- clim.nc$dim$time$len
+
+  #clim.nc = nc_open("/data/Aqua/climatology/8_day/8_day_climatology.nc4")
+  #e.day <- ceiling(((as.numeric(jday) + as.numeric(ejday)) / 2)/8)
   #clim.nc = nc_open("/Users/leathers/Downloads/8_day_climatology.nc4")
   #e.day <- 39
-  sst.c <- ncvar_get(clim.nc, "sst",start = c(1,1,e.day), count = c(clim.nc$dim$lon$len,clim.nc$dim$lat$len,1))
+  sst.c <- ncvar_get(clim.nc, "sst",start = c(1,1,as.numeric(jday)), count = c(clim.nc$dim$lon$len,clim.nc$dim$lat$len,1))
   lon.c <- ncvar_get(clim.nc, "lon")
   lat.c <- ncvar_get(clim.nc, "lat")
   nc_close(clim.nc)
@@ -111,6 +134,11 @@ library(grid)
   # compute the difference and plot
   diff = aqua.raster - clim.raster
   
+  # write out diff raster to netcdf 
+  outname = substr(fname, 2, 90)
+  print(fname)
+  writeRaster(diff, paste0("/home/james/anomalies/data/SSTanomaly_", outname),  overwrite=TRUE, format="CDF", varname="SST", varunit="Celsius", 
+              longname="Sea Surface Temperature", xname="lon", yname="lat")
   ###### PLOTTING ###### 
   
   #Color palette creation
@@ -240,23 +268,24 @@ library(grid)
   
   # Begin the PNG
   # max pixels = range lat / .00980196 * range lon / .00980196
-  png(filename = "/home/james/anomalies/anomaly.png",width = 10, height = 7.6, units = "in", res = 200)
+  png(filename = "/home/james/anomalies/AquaAtlantic_SST_anomaly.png",width = 8, height = 6, units = "in", res = 200)
     lp = levelplot(diff, margin = FALSE, maxpixels = 18075029, xlab = "Longitude", ylab = "Latitude", 
-                   main=paste0("Aqua 8-Day SST Anomalies ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr), cex=2) +
-    layer(sp.polygons(usa, fill='gray16')) + layer(sp.polygons(can, fill='gray16')) + layer(sp.polygons(mex, fill='gray16')) + 
-    layer(sp.polygons(cub, fill='gray16')) + layer(sp.polygons(pr, fill='gray16')) + layer(sp.polygons(jam, fill='gray16')) + 
-    layer(sp.polygons(bel, fill='gray16')) + layer(sp.polygons(bah, fill='gray16')) + layer(sp.polygons(hai, fill='gray16')) + 
-    layer(sp.polygons(cay, fill='gray16')) + layer(sp.polygons(dr, fill='gray16')) + layer(sp.polygons(vi, fill='gray16')) +
-    layer(sp.polygons(tc, fill='gray16'))  + layer(sp.polygons(ang, fill='gray16')) + layer(sp.polygons(gua, fill = 'gray16')) +
+                   main=paste0("MODIS-Aqua 8-Day SST Anomalies ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr), cex=2) +
+    #layer(sp.polygons(usa, fill='gray16')) + layer(sp.polygons(can, fill='gray16')) + layer(sp.polygons(mex, fill='gray16')) + 
+    #layer(sp.polygons(cub, fill='gray16')) + layer(sp.polygons(pr, fill='gray16')) + layer(sp.polygons(jam, fill='gray16')) + 
+    #layer(sp.polygons(bel, fill='gray16')) + layer(sp.polygons(bah, fill='gray16')) + layer(sp.polygons(hai, fill='gray16')) + 
+    #layer(sp.polygons(cay, fill='gray16')) + layer(sp.polygons(dr, fill='gray16')) + layer(sp.polygons(vi, fill='gray16')) +
+    #layer(sp.polygons(tc, fill='gray16'))  + layer(sp.polygons(ang, fill='gray16')) + layer(sp.polygons(gua, fill = 'gray16')) +
     layer(sp.polygons(hon, fill='gray16'))
     diverge0(lp)
+    copyright.draw("", im1, .93, .96, 1, fontsize = 8)
+    copyright.draw("", im2, .02, .96, 1, fontsize = 8) 
     trellis.focus("legend", side="right", clipp.off=TRUE, highlight=FALSE)
     grid::grid.text('DegC', y=unit(-.015, "npc"), 
-                    x=unit(.26, "npc"), gp=gpar(fontsize=8))  
+                    x=unit(.26, "npc"), gp=gpar(fontsize=8))
     trellis.unfocus()
   garbage <- dev.off()
 
-  
   
   
   
@@ -317,11 +346,11 @@ library(grid)
   df <- data.frame(map.p)
   #Make appropriate column headings
   colnames(df) <- c("Longitude", "Latitude", "MAP")
-  
-  png(filename = "/home/james/anomalies/pass.aqua.png",width = 10, height = 7.6, units = "in", res = 400)
-  ggplot(data=df, aes(y=Latitude, x=Longitude)) +
+
+  png(filename = "/home/james/anomalies/AquaAtlantic_SST.png",width = 8, height = 6, units = "in", res = 200)
+  gg = ggplot(data=df, aes(y=Latitude, x=Longitude)) +
     geom_raster(aes(fill=MAP)) +
-    ggtitle(paste0("8-Day Aqua SST ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr)) +
+    ggtitle(paste0("MODIS-Aqua 8-Day SST ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr)) +
     #geom_polygon(data = land, aes(x=long, y = lat, group = group), 
     #fill = "gray16") +
     
@@ -384,6 +413,14 @@ library(grid)
     coord_fixed(xlim = c(-100, -55),  ylim = c(18, 48), ratio = 1.2)
   
   garbage <- dev.off()
+  
+  cema <- image_read("/home/sat_ops/goes_r/nexrad/cema38.png")
+  udel <- image_read("/home/sat_ops/goes_r/nexrad/udel38.png")
+  # add logos to ggplot
+  plot <- image_read("/home/james/anomalies/AquaAtlantic_SST.png")
+  final_plot  <- image_composite(plot, cema, offset = "+1540+5")
+  final_plot  <- image_composite(final_plot, udel, offset = "+5+5")
+  image_write(final_plot, "/home/james/anomalies/AquaAtlantic_SST.png")
   
   
   # CLIM
@@ -392,10 +429,10 @@ library(grid)
   #Make appropriate column headings
   colnames(df) <- c("Longitude", "Latitude", "MAP")
   
-  png(filename = "/home/james/anomalies/climatology.png",width = 10, height = 7.6, units = "in", res = 400)
+  png(filename = "/home/james/anomalies/AquaAtlantic_SST_climatology.png",width = 8, height = 6, units = "in", res = 200)
   ggplot(data=df, aes(y=Latitude, x=Longitude)) +
     geom_raster(aes(fill=MAP)) +
-    ggtitle(paste0("8-Day SST Climatology ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr)) +
+    ggtitle(paste0("MODIS-Aqua 8-Day SST Climatology ", emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr)) +
     #geom_polygon(data = land, aes(x=long, y = lat, group = group), 
     #fill = "gray16") +
     
@@ -459,10 +496,18 @@ library(grid)
   
   garbage <- dev.off()
   
+  # add logos to ggplot
+  plot <- image_read("/home/james/anomalies/AquaAtlantic_SST_climatology.png")
+  final_plot  <- image_composite(plot, cema, offset = "+1540+5")
+  final_plot  <- image_composite(final_plot, udel, offset = "+5+5")
+  image_write(final_plot, "/home/james/anomalies/AquaAtlantic_SST_climatology.png")
+  
   diff.hist = hist(diff, plot = FALSE)
   
-  png(filename = "/home/james/anomalies/hist.anom.png",width = 10, height = 7.6, units = "in", res = 400)
-  plot(diff.hist, main = paste0(emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr, " Anomaly Histogram"), xlab = "Sea Surface Temperature (DegC)")
+  png(filename = "/home/james/anomalies/AquaAtlantic_SST_histogram.png",width = 8, height = 6, units = "in", res = 200)
+  plot(diff.hist, main = paste0(emn, "/", edom, "/", eyr, "-", mn, "/", dom, "/", yr, " MODIS-Aqua Anomaly Histogram"), xlab = "Sea Surface Temperature (DegC)")
+  copyright.draw("", im1, .93, .96, 1, fontsize = 8)
+  copyright.draw("", im2, .02, .96, 1, fontsize = 8) 
   garbage <- dev.off()
   
   cat(1)
