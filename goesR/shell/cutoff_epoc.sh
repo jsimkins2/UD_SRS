4fb08e4e837b98fdeec7d583f5c2a0b7a54e71b2
Default Seadas User <seadas-user@localhost>
2018-04-05 12:50:35 -0400

    backing up cutoff and tc

--- goesR/shell/cutoff_epoc.sh
+++ goesR/shell/cutoff_epoc.sh
@@ -1,5 +1,5 @@
 
-find /home/sat_ops/goes_r/cloud_prod/3* -type f -mmin -220 -exec cp {} /home/sat_ops/goes_r/cloud_prod/noaa_format/data/ \;
+find /home/sat_ops/goes_r/cloud_prod/ -type f -name '3*' -mmin -220 -exec cp {} /home/sat_ops/goes_r/cloud_prod/noaa_format/data/ \;
 
 FILES=/home/sat_ops/goes_r/cloud_prod/noaa_format/data/3*
 for f in $FILES
@@ -14,7 +14,14 @@ do
   fi
 done
 
-find /home/sat_ops/goes_r/cloud_prod/noaa_format/data/OR* -type f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/cloud_prod/noaa_format/data/OR* -type f -mmin +500 -exec rm -f {} \;
 
-find /home/sat_ops/goes_r/cloud_prod/noaa_format/image_conus/* -type f -mmin +220 -exec rm -f {} \;
-find /home/sat_ops/goes_r/nexrad/image_nxrd_goes/* -type f -mmin +220 -exec rm -f {} \;
+
+find /home/sat_ops/goes_r/cloud_prod/noaa_format/image_conus/* -type f -mmin +500 -exec rm -f {} \;
+find /home/sat_ops/goes_r/cloud_prod/noaa_format/image_midatlantic/* -type f -mmin +500 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/image_kdox_goes/* -tiype f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/image_kdix_goes/* -type f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/image_klwx_goes/* -type f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/data_kdox/* -tiype f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/data_kdix/* -type f -mmin +220 -exec rm -f {} \;
+find /home/sat_ops/goes_r/nexrad/data_klwx/* -type f -mmin +220 -exec rm -f {} \;
--- goesR/shell/tc_conus.sh
+++ goesR/shell/tc_conus.sh
@@ -5,13 +5,26 @@ dir=/home/sat_ops/goes_r/cloud_prod/noaa_format/data/
 (cd $dir ; ls *C01*) | tail -30 > /home/sat_ops/goes_r/cloud_prod/noaa_format/C01_logfile.txt
 (cd $dir ; ls *C02*) | tail -30 > /home/sat_ops/goes_r/cloud_prod/noaa_format/C02_logfile.txt
 (cd $dir ; ls *C03*) | tail -30 > /home/sat_ops/goes_r/cloud_prod/noaa_format/C03_logfile.txt
+(cd $dir ; ls *C13*) | tail -30 > /home/sat_ops/goes_r/cloud_prod/noaa_format/C13_logfile.txt
 
-python /home/sat_ops/goes_r/cloud_prod/noaa_format/goes16_conus_tc_png.py
+python /home/sat_ops/goes_r/cloud_prod/noaa_format/conus_goes16_TC.py
 
-ls /home/sat_ops/goes_r/cloud_prod/noaa_format/image_conus/ | tail -30 > /home/sat_ops/goes_r/cloud_prod/noaa_format/img_logfile.txt
+# Make the gif from the PNGs we just created above
+ls /home/sat_ops/goes_r/cloud_prod/noaa_format/image_conus/ | tail -72 > /home/sat_ops/goes_r/cloud_prod/noaa_format/img_logfile.txt
 
 python /home/sat_ops/goes_r/cloud_prod/noaa_format/gifmaker_goes16.py
 
-source deactivate
-
 scp /home/sat_ops/goes_r/cloud_prod/noaa_format/conus_goes16.gif /var/www/html/imagery/
+
+
+# Now make a midatlantic version of the code above
+
+python /home/sat_ops/goes_r/cloud_prod/noaa_format/MidAtlantic_goes16TC.py
+
+ls /home/sat_ops/goes_r/cloud_prod/noaa_format/image_midatlantic/ | tail -72 > /home/sat_ops/goes_r/cloud_prod/noaa_format/MAimg_logfile.txt
+
+python /home/sat_ops/goes_r/cloud_prod/noaa_format/gifmaker_MidAtlantic.py
+
+scp /home/sat_ops/goes_r/cloud_prod/noaa_format/MidAtlantic_goes16_TC.gif /var/www/html/imagery/
+
+source deactivate
