094dd6584e7c9171738be715e63658d76ffc9d67
James Simkins <jamessimkins5@gmail.com>
2019-03-01 10:34:05 -0500

    pushing files to github

--- polar/roffs/goes16_dineof.py
+++ polar/roffs/goes16_dineof.py
@@ -7,8 +7,10 @@ lon_bounds = [-98, -80]
 
 # grab sst data from the last 3 days and use DQF == 0 data
 goes_nc = xr.open_dataset("http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
-goes_nc = goes_nc.sel(latitude=slice(18.0,31.0), longitude=slice(-98,-80))
+goes_nc = goes_nc.sel(latitude=slice(18.0,31.0), longitude=slice(-98,-80), 
+          time=slice('2018-10-30'))
 
+goes_nc = goes_nc.resample(time='1D').mean('time')
 #aggregate daily sst
 sst1 = goes_nc['SST'][-1]
 dqf1 = goes_nc['DQF'][-1]
