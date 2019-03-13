69d5fccfa2682ca2833c61940894b010af2bd788
jsimkins2 <jamessimkins5@gmail.com>
2019-03-13 15:21:33 -0400

    quality check good to go

--- sst_goes16/quality_check.py
+++ sst_goes16/quality_check.py
@@ -1,35 +1,68 @@
-# goes16 quality control algorithm
-lat_bounds = [23, 27]
-lon_bounds = [-81, -77]
-# Biscayne Bay 
-# grab sst data from the last 3 days and use DQF == 0 data
-goes_nc = xr.open_dataset("http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
-#goes_nc = goes_nc.sel(longitude=[-90], method='nearest')
-#sst = sst.sel(longitude=[-51], method='nearest')
-
-
+# goes16 sst quality control algorithm
 import xarray as xr
 import metpy
 import datetime
+from os import listdir
+from os.path import isfile, join
+import os
+import sys
 
+# test - seems that the lower right hand corner is a solid region as far as data goes.
+# the algorithm processes from North Pole to South Pole and many times the failed datasets
+# are due to lack of time for algorithm to complete. Thus, we check the lower right hand corner.
+# we will check this region for gaps in the data. Instead of deleting the passes, let's just 
+# add a suspect folder and place them in there so they aren't just deleted
+
+
+nowdate = datetime.utcnow()
+datadir = "/home/sat_ops/goesR/data/sst/" + str(nowdate.year) + "/"
+filenames = [f for f in listdir(datadir) if isfile(join(datadir, f))]
+
+badfiles = []
+
+for f in range(0,len(filenames)):
+    ds = xr.open_dataset(datadir + filenames[f])
+    sst = ds.metpy.parse_cf("SST")
+    sst = sst.sel(longitude=slice(-88, -50), latitude=slice(16,18))
+    sst = sst.where(sst.values > 270, np.nan)
+    sst = sst.fillna(-999)
+    if np.percentile(sst.values, 60) == -999:
+        move_files = "mv " + datadir + filenames[f] + "/home/sat_ops/goesR/data/sst/suspect/"
+        os.system(move_files)
+    if np.percentile(sst.values, 60) != -999:
+        dqf = ds.metpy.parse_cf("DQF")
+        dqf = dqf.sel(longitude=slice(-65, -50), latitude=slice(37,45))
+        dqf = dqf.where(dqf.values > 1, np.nan)
+        dqf = dqf.fillna(-999)
+        if np.percentile(dqf.values, 60) == -999:
+            move_files = "mv " + datadir + filenames[f] + "/home/sat_ops/goesR/data/sst/suspect/"
+            os.system(move_files)
+
+
+'''
 badfiles2 = []
 ds = xr.open_dataset("http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
 for t in range(0,len(ds.time.values)):
     sst = ds.metpy.parse_cf("SST")[t]
     #sst = sst.sel(time=datetime.datetime.strptime('2019-03-08 16:28:18', '%Y-%m-%d %H:%M:%S'))
-    sst = sst.sel(longitude=slice(-65, -50), latitude=slice(16,20))
+    sst = sst.sel(longitude=slice(-88, -50), latitude=slice(16,18))
     sst = sst.where(sst.values > 270, np.nan)
     sst = sst.fillna(-999)
     if np.percentile(sst.values, 60) == -999:
-        badfiles2.append(sst.time.values)
-    
+        badfiles.append(sst.time.values)
 
 
 
+ds = xr.open_dataset("http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
+for t in range(0,len(ds.time.values)):
+    dqf = ds.metpy.parse_cf("DQF")[t]
+    dqf = dqf.sel(longitude=slice(-65, -50), latitude=slice(37,45))
+    dqf = dqf.where(dqf.values > 1, np.nan)
+    dqf = dqf.fillna(-999)
+    if np.percentile(dqf.values, 60) == -999:
+        badfiles2.append(dqf.time.values)
 
-
-#
-from itertools import groupby
-[len(list(v)) for k,v in groupby(sst.values) if k==-999]
+#from itertools import groupby
+#[len(list(v)) for k,v in groupby(sst.values) if k==-999]
 
 
--- sst_goes16/save_reprojected_sst.py
+++ sst_goes16/save_reprojected_sst.py
@@ -37,98 +37,99 @@ for dataset in filenames:
     else:
         dataset_name = str(dataset)
     if os.path.isfile("/data/GOES/GOES-R/sst/" + str(nowdate.year) + "/" + str(dataset_name)) == False:
-        print(str(dataset))
-        d = Dataset("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" + dataset)
-        ds = NetCDF4DataStore(d)
-        ds = xr.open_dataset(ds)
-        dat = ds.metpy.parse_cf('SST')
-        proj = dat.metpy.cartopy_crs
-        dat_dqf  = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_dqf/" + str(nowdate.year) + "/DQF" + str(dataset))
-        new = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_sst/" + str(nowdate.year) + "/SST" + str(dataset))
-        
-        dat15 = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_b15/" + str(nowdate.year) + "/B15" + str(dataset))
-        
-        new = new.where((new['latitude']>16) & (new['latitude']<52) & (new['longitude']>-100) & (new['longitude']<-50), drop=True)
-        dat_dqf = dat_dqf.where((dat_dqf['latitude']>16) & (dat_dqf['latitude']<52) & (dat_dqf['longitude']>-100) & (dat_dqf['longitude']<-50), drop=True)
-        dat15 = dat15.where((dat15['latitude']>16) & (dat15['latitude']<52) & (dat15['longitude']>-100) & (dat15['longitude']<-50), drop=True)
-
-        # now write it all to netcdf!
-        f = Dataset("/data/GOES/GOES-R/sst/" + str(nowdate.year) + "/" + dataset_name,'w', format='NETCDF4') #'w' stands for write
-        # dimensions
-        f.createDimension('longitude', new['longitude'].size)
-        f.createDimension('latitude', new['latitude'].size)
-        f.createDimension('time', 1)
-        
-        # variables
-        longitude = f.createVariable('longitude', 'f4', 'longitude')
-        longitude.standard_name = new['longitude'].long_name
-        longitude.units = new['longitude'].units
-        
-        latitude = f.createVariable('latitude', 'f4', 'latitude')
-        latitude.standard_name = new['latitude'].long_name
-        latitude.units = new['latitude'].units
-        
-        time = f.createVariable('time', 'f8', 'time')
-        time.standard_name = "time"
-        time.long_name = "EPOCH Time"
-        time.units = "seconds since 1970-01-01T00:00:00Z"
-        
-        proj = f.createVariable('projection', 'f4')
-        proj.proj4_string = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-        proj.epsg_code = 4326
-        
-        sst = f.createVariable('SST', 'f4', ('time', 'latitude', 'longitude'))
-        sst.long_name = d.variables['SST'].long_name
-        sst.standard_name = d.variables['SST'].standard_name
-        sst.units = d.variables['SST'].units
-        sst.resolution = d.variables['SST'].resolution
-        sst.missing_value = -999.0
-        
-        dqf = f.createVariable('DQF', 'f4', ('time', 'latitude', 'longitude'))
-        dqf.long_name = d.variables['DQF'].long_name
-        dqf.standard_name = d.variables['DQF'].standard_name
-        dqf.units = d.variables['DQF'].units
-        dqf.flag_values = d.variables['DQF'].flag_values
-        dqf.flag_meanings = d.variables['DQF'].flag_meanings
-        
+        if os.path.isfile("/data/GOES/GOES-R/sst/suspect/"+ str(dataset_name)) == False:
+            print(str(dataset))
+            d = Dataset("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" + dataset)
+            ds = NetCDF4DataStore(d)
+            ds = xr.open_dataset(ds)
+            dat = ds.metpy.parse_cf('SST')
+            proj = dat.metpy.cartopy_crs
+            dat_dqf  = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_dqf/" + str(nowdate.year) + "/DQF" + str(dataset))
+            new = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_sst/" + str(nowdate.year) + "/SST" + str(dataset))
+            
+            dat15 = xr.open_dataset("/home/sat_ops/goesR/data/sst/rast_b15/" + str(nowdate.year) + "/B15" + str(dataset))
+            
+            new = new.where((new['latitude']>16) & (new['latitude']<52) & (new['longitude']>-100) & (new['longitude']<-50), drop=True)
+            dat_dqf = dat_dqf.where((dat_dqf['latitude']>16) & (dat_dqf['latitude']<52) & (dat_dqf['longitude']>-100) & (dat_dqf['longitude']<-50), drop=True)
+            dat15 = dat15.where((dat15['latitude']>16) & (dat15['latitude']<52) & (dat15['longitude']>-100) & (dat15['longitude']<-50), drop=True)
     
-        band15 = f.createVariable('Band15', 'f4', ('time', 'latitude', 'longitude'))
-        band15.long_name = "GOES-16 Band 15 Brightness Temperature"
-        band15.standard_name = "brightness_temperature"
-        band15.units = "kelvin"
-        band15.valid_min = 0
-        band15.valid_max = 4095
+            # now write it all to netcdf!
+            f = Dataset("/data/GOES/GOES-R/sst/" + str(nowdate.year) + "/" + dataset_name,'w', format='NETCDF4') #'w' stands for write
+            # dimensions
+            f.createDimension('longitude', new['longitude'].size)
+            f.createDimension('latitude', new['latitude'].size)
+            f.createDimension('time', 1)
+            
+            # variables
+            longitude = f.createVariable('longitude', 'f4', 'longitude')
+            longitude.standard_name = new['longitude'].long_name
+            longitude.units = new['longitude'].units
+            
+            latitude = f.createVariable('latitude', 'f4', 'latitude')
+            latitude.standard_name = new['latitude'].long_name
+            latitude.units = new['latitude'].units
+            
+            time = f.createVariable('time', 'f8', 'time')
+            time.standard_name = "time"
+            time.long_name = "EPOCH Time"
+            time.units = "seconds since 1970-01-01T00:00:00Z"
+            
+            proj = f.createVariable('projection', 'f4')
+            proj.proj4_string = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+            proj.epsg_code = 4326
+            
+            sst = f.createVariable('SST', 'f4', ('time', 'latitude', 'longitude'))
+            sst.long_name = d.variables['SST'].long_name
+            sst.standard_name = d.variables['SST'].standard_name
+            sst.units = d.variables['SST'].units
+            sst.resolution = d.variables['SST'].resolution
+            sst.missing_value = -999.0
+            
+            dqf = f.createVariable('DQF', 'f4', ('time', 'latitude', 'longitude'))
+            dqf.long_name = d.variables['DQF'].long_name
+            dqf.standard_name = d.variables['DQF'].standard_name
+            dqf.units = d.variables['DQF'].units
+            dqf.flag_values = d.variables['DQF'].flag_values
+            dqf.flag_meanings = d.variables['DQF'].flag_meanings
+            
         
-        # data 
-        latitude[:] = new['latitude'].values
-        longitude[:] = new['longitude'].values
-        sst[:]= new['sst'].values
-        dqf[:]=dat_dqf['dqf'].values
-        band15[:]=dat15['b15'].values
-        time[:] = (dat['time'].values.astype('uint64') / 1e9).astype('uint32')
-        
-        # metadata
-        f.creator_name = "James Simkins"
-        f.creator_email = "simkins@udel.edu"
-        f.institution = "University of Delaware Ocean Exploration, Remote Sensing and Biogeography Group (ORB)"
-        f.url = "http://orb.ceoe.udel.edu/"
-        f.source = "satellite observation NASA MODIS-Aqua instrument"
-        f.groundstation = "University of Delaware, Newark, Center for Remote Sensing"
-        f.software = 0.0
-        f.inputMET1 = 0.0
-        f.inputOZONE1 = 0.0
-        f.inputCalibrationFile = 0.0
-        f.product_list = "SST, DQF"
-        f.summary = "GOES16 SST product, reprojected to EPSG:4326."
-        
-        f.close()
-        # add netcdf file compression.  -L 5 means level 5 compression, -O means overwrite
-        fname = "/data/GOES/GOES-R/sst/" + str(nowdate.year) + "/" + str(dataset_name)
-        temp_str = "ncks " + fname + " " + fname + " -L 5 -O"
-        os.system(temp_str)
-        
-        flip_lat = "Rscript /home/sat_ops/goesR/data/sst/scripts/flip_lat_sst.R " + fname
-        os.system(flip_lat)
-
-    else:
-        print('all caught up!')
+            band15 = f.createVariable('Band15', 'f4', ('time', 'latitude', 'longitude'))
+            band15.long_name = "GOES-16 Band 15 Brightness Temperature"
+            band15.standard_name = "brightness_temperature"
+            band15.units = "kelvin"
+            band15.valid_min = 0
+            band15.valid_max = 4095
+            
+            # data 
+            latitude[:] = new['latitude'].values
+            longitude[:] = new['longitude'].values
+            sst[:]= new['sst'].values
+            dqf[:]=dat_dqf['dqf'].values
+            band15[:]=dat15['b15'].values
+            time[:] = (dat['time'].values.astype('uint64') / 1e9).astype('uint32')
+            
+            # metadata
+            f.creator_name = "James Simkins"
+            f.creator_email = "simkins@udel.edu"
+            f.institution = "University of Delaware Ocean Exploration, Remote Sensing and Biogeography Group (ORB)"
+            f.url = "http://orb.ceoe.udel.edu/"
+            f.source = "satellite observation NASA MODIS-Aqua instrument"
+            f.groundstation = "University of Delaware, Newark, Center for Remote Sensing"
+            f.software = 0.0
+            f.inputMET1 = 0.0
+            f.inputOZONE1 = 0.0
+            f.inputCalibrationFile = 0.0
+            f.product_list = "SST, DQF"
+            f.summary = "GOES16 SST product, reprojected to EPSG:4326."
+            
+            f.close()
+            # add netcdf file compression.  -L 5 means level 5 compression, -O means overwrite
+            fname = "/data/GOES/GOES-R/sst/" + str(nowdate.year) + "/" + str(dataset_name)
+            temp_str = "ncks " + fname + " " + fname + " -L 5 -O"
+            os.system(temp_str)
+            
+            flip_lat = "Rscript /home/sat_ops/goesR/data/sst/scripts/flip_lat_sst.R " + fname
+            os.system(flip_lat)
+    
+        else:
+            print('all caught up!')
