27bd43e282e35d988479d5b4ff07bd72d5688411
jsimkins2 <jamessimkins5@gmail.com>
2019-04-01 11:44:11 -0400

    rerunning daily composite

--- sst_goes16/daily_composite.py
+++ sst_goes16/daily_composite.py
@@ -7,7 +7,7 @@ import pandas as pd
 # paths
 outpath = "/data/GOES/GOES-R/1day/"
 
-datelist = pd.date_range('2019-10-30',pd.datetime.today()).tolist()
+datelist = pd.date_range('2018-10-30',pd.datetime.today()).tolist()
 for d in range(0,len(datelist)):
     print(datelist[d])
     goes_nc = xr.open_dataset("http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
