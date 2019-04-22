3ecc10036e0409b4387c084c233ec88a3e2077d8
jsimkins2 <jamessimkins5@gmail.com>
2019-04-22 12:19:05 -0400

    special download

--- goes16/special_cases/special_download.py
+++ goes16/special_cases/special_download.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import os.path
 import os
 
-# open a client 
+# open a client
 client = storage.Client()
 
 # ping the bucket
@@ -15,7 +15,7 @@ bucket = client.get_bucket('gcp-public-data-goes-16')
 now = datetime.utcnow()
 tt = now.timetuple()
 
-jday = range(254, 261)
+jday = range(108, 112)
 hour = range(0,24)
 
 for j in jday:
@@ -23,25 +23,25 @@ for j in jday:
         # hours has 0 in the filename
         if len(str(h)) == 1:
             hourstr = '0' + str(h)
-        
+
         if len(str(h)) == 2:
             hourstr = str(h)
-        
-                    
-        blobs = bucket.list_blobs(prefix='ABI-L2-MCMIPC/'+ str(2018) + '/' + str(j) + '/' + hourstr + '/')
-        print blobs
+
+
+        blobs = bucket.list_blobs(prefix='ABI-L2-MCMIPF/'+ str(now.year) + '/' + jday + '/' + hourstr + '/')
         results = []
         for i in blobs:
             results.append(i)
-            for r in results:
+            for j in results:
                 # parse for the filename we want
-                temname = str(r)[58:-1]
-                print temname
+                temname = str(j)[58:-1]
+                orig_temname = temname
+                # download said file but change naming structure to allow for chronological sorting regardless of scan mode
+                if temname.split('-')[3][0:2] != 'M3':
+                    temname = temname.split('-')[0] + '-' + temname.split('-')[1] + '-' + temname.split('-')[2] + '-' + 'M3' + temname.split('-')[3][2:len(temname)]
+                else:
+                    temname = str(temname)
                 # if the file already exists, do NOT download and overwrite it
-                # adding the 3223350605. so parsing works downstream, should probably change this later
-                if os.path.isfile("/home/sat_ops/goesR/special_cases/florence/data/" + temname.split("_e")[0] + ".nc") == False:
-                    # call the individual file we want
-                    goesfile= bucket.get_blob('ABI-L2-MCMIPC/'+ str(2018) + '/' + str(j) + '/' + hourstr + '/' + temname)
-                    # download said file and keep original naming structure
-                    goesfile.download_to_filename("/home/sat_ops/goesR/special_cases/florence/data/" + temname.split("_e")[0] + ".nc") 
-                    print "Downloading " + temname
+                if os.path.isfile("/home/sat_ops/goesR/data/sst/temp/" + temname.split("_e")[0] + ".nc") == False:
+                    goesfile= bucket.get_blob('ABI-L2-MCMIPF/'+ str(now.year) + '/' + jday + '/' + hourstr + '/' + orig_temname)
+                    goesfile.download_to_filename("/home/sat_ops/goesR/data/sst/temp/" + temname.split("_e")[0] + ".nc")
