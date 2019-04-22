b82ef43105ee0d7c2b7ca5093bd1c757a7790ebd
jsimkins2 <jamessimkins5@gmail.com>
2019-04-22 09:25:21 -0400

    asd

--- sst_goes16/save_raw_sst.py
+++ sst_goes16/save_raw_sst.py
@@ -19,12 +19,15 @@ import time
 from time import mktime
 import os.path
 import os
-import numpy as np 
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 
 nowdate = datetime.utcnow()
-cat = TDSCatalog('https://thredds.ucar.edu/thredds/catalog/satellite/goes16/GOES16/Products/SeaSurfaceTemperature/FullDisk/' + \
+
+# Main Server
+#https://thredds.ucar.edu/thredds/catalog/satellite/goes16/GOES16/Products/SeaSurfaceTemperature/FullDisk/
+cat = TDSCatalog('https://thredds-test.unidata.ucar.edu/thredds/catalog/satellite/goes16/GOES16/Products/SeaSurfaceTemperature/FullDisk/' + \
                   str(nowdate.year) + str("%02d"%nowdate.month) + str("%02d"%nowdate.day) + '/catalog.xml')
 dataset_name = sorted(cat.datasets.keys())[-1]
 dataset = cat.datasets[dataset_name]
@@ -39,10 +42,10 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     dat = ds.metpy.parse_cf('SST')
     proj = dat.metpy.cartopy_crs
     dat_dqf = ds.metpy.parse_cf('DQF')
-    
+
     dat = dat.where(dat > -1)
     dat.values[np.isnan(dat.values)] = -999
-    
+
     # Now grab band 15
     filenames = [f for f in listdir("/home/sat_ops/goesR/data/fulldisk/") if isfile(join("/home/sat_ops/goesR/data/fulldisk/", f))]
     ds = Dataset("/home/sat_ops/goesR/data/fulldisk/" + filenames[-1])
@@ -50,27 +53,27 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     ds = xr.open_dataset(ds)
     d2 = ds
     dat15 = d2.metpy.parse_cf('CMI_C15')
-    
+
     f = Dataset("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" + str(dataset),'w', format='NETCDF4') #'w' stands for write
     # dimensions
     f.createDimension('x', dat['x'].size)
     f.createDimension('y', dat['y'].size)
     f.createDimension('time', 1)
-    
+
     # variables
-    
+
     x = f.createVariable('x', 'f4', 'x')
     x.standard_name = dat['x'].standard_name
     x.units = dat['x'].units
-    
+
     y = f.createVariable('y', 'f4', 'y')
     y.standard_name = dat['y'].standard_name
     y.units = dat['y'].units
-    
+
     time = f.createVariable('time', 'f8', 'time')
     time.standard_name = dat['t'].standard_name
     time.long_name = dat['t'].long_name
-    
+
     proj = f.createVariable('goes_imager_projection', 'f4')
     proj.grid_mapping_name = d.variables['goes_imager_projection'].grid_mapping_name
     proj.perspective_point_height = d.variables['goes_imager_projection'].perspective_point_height
@@ -80,7 +83,7 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     proj.latitude_of_projection_origin = d.variables['goes_imager_projection'].latitude_of_projection_origin
     proj.longitude_of_projection_origin = d.variables['goes_imager_projection'].longitude_of_projection_origin
     proj.sweep_angle_axis = d.variables['goes_imager_projection'].sweep_angle_axis
-    
+
     sst = f.createVariable('SST', 'f4', ('time', 'y', 'x'))
     sst.long_name = d.variables['SST'].long_name
     sst.standard_name = d.variables['SST'].standard_name
@@ -88,7 +91,7 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     sst.resolution = d.variables['SST'].resolution
     sst.missing_value = d.variables['SST']._FillValue
     sst.grid_mapping = d.variables['SST'].grid_mapping
-    
+
     dqf = f.createVariable('DQF', 'f4', ('time', 'y', 'x'))
     dqf.long_name = d.variables['DQF'].long_name
     dqf.standard_name = d.variables['DQF'].standard_name
@@ -96,13 +99,13 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     dqf.flag_values = d.variables['DQF'].flag_values
     dqf.flag_meanings = d.variables['DQF'].flag_meanings
     dqf.grid_mapping = d.variables['DQF'].grid_mapping
-    
+
     band15 = f.createVariable('Band15', 'f4', ('time', 'y', 'x'))
     band15.long_name = dat15.long_name
     band15.standard_name = dat15.standard_name
     band15.units = dat15.units
 
-    # data 
+    # data
     x[:] = dat['x'].values
     y[:] = dat['y'].values
     sst[:]= dat.values
@@ -112,10 +115,3 @@ if os.path.isfile("/home/sat_ops/goesR/data/sst/raw/" + str(nowdate.year) + "/" 
     f.close()
 else:
     print('no new file')
-
-
-
-
-
-
-
