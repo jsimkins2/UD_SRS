782f657c30d6c82585b34cf74926ab83006a3693
jsimkins2 <jamessimkins5@gmail.com>
2019-04-25 13:13:05 -0400

    asd

--- sst_goes16/save_reprojected_sst.py
+++ sst_goes16/save_reprojected_sst.py
@@ -211,12 +211,25 @@ goes_nc = xr.open_dataset(
     "http://basin.ceoe.udel.edu/thredds/dodsC/goes_r_sst.nc")
 # grab the last 24 hours of sst dataset
 goes_nc = goes_nc.isel(time=range(-24, 0))
+newtimestamp = goes_nc.time.values[-1]
 goes_nc = goes_nc.drop('Band15')
 for t in range(len(goes_nc.time.values)):
     x = goes_nc['SST'][t]
     goes_nc['SST'][t] = x.where(goes_nc['DQF'][t] == 0)
 
-goes_nc = goes_nc.resample(time='2D').mean('time')
+goes_nc = goes_nc.drop(['DQF'])
+goes_nc = goes_nc.mean('time')
+goes_nc['sst'] = goes_nc['SST']
+goes_nc = goes_nc.drop(['SST'])
+
+coords = {'latitude':goes_nc.latitude, 'longitude':goes_nc.longitude, 'projection':goes_nc.projection}
+coords['time'] = newtimestamp
+goes_nc=goes_nc.drop(['projection'])
+goes_nc = xr.DataArray(goes_nc['sst'].values, dims = ['longitude', 'latitude'])
+goes_nc = xr.concat([goes_nc], 'time')
+goes_nc = goes_nc.to_dataset(name='sst')
 outpath = "/data/GOES/GOES-R/rolling_1day/"
 goes_nc.to_netcdf(path=outpath + 'GOES16_SST_rolling_1day.nc',
                   format='NETCDF3_CLASSIC')
+
+goes_nc['sst'].plot()
