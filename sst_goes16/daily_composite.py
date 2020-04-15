87aad1e28b667315f8382272189bf06d480aee14
Default Seadas User <seadas-user@localhost>
2020-04-15 14:19:47 -0400

    adding daily composite py file

--- sst_goes16/daily_composite.py
+++ sst_goes16/daily_composite.py
@@ -10,9 +10,9 @@ outpath2 = "/data/GOES/GOES-R/daily_composite/"
 datelist = pd.date_range('2018-01-01', pd.datetime.today()).tolist()
 for d in range(0, len(datelist)):
     print(datelist[d])
-    goes_nc = xr.open_dataset(
+    goes_main = xr.open_dataset(
         "http://basin.ceoe.udel.edu/thredds/dodsC/GOESJPLSST.nc")
-    goes_nc = goes_nc.sel(time=datetime.strftime(
+    goes_nc = goes_main.sel(time=datetime.strftime(
         datelist[d].date(), '%Y-%m-%d'))
     goes_nc = goes_nc.drop('projection')
     goes_nc = goes_nc.drop('dt_analysis')
@@ -31,19 +31,21 @@ for d in range(0, len(datelist)):
     
     goes_nc['sst'] = goes_nc['sea_surface_temperature']
     goes_nc = goes_nc.drop(['sea_surface_temperature'])
+    # have to add the following line becuase of a weird xarray netcdf4 error when writing the xarray to netcdf
+    del goes_nc.attrs['_NCProperties']
     goes_nc.to_netcdf(path=outpath1 + '/' + str(datelist[d].year) + '/GOES16_SST_1day_' + str(datelist[d].year) + str("{0:0=3d}".format(
-        datelist[d].dayofyear)) + '_' + str("{0:0=2d}".format(datelist[d].month)) + str("{0:0=2d}".format(datelist[d].day)) + '.nc', format='NETCDF3_CLASSIC')
+        datelist[d].dayofyear)) + '_' + str("{0:0=2d}".format(datelist[d].month)) + str("{0:0=2d}".format(datelist[d].day)) + '.nc', mode='w',format='NETCDF4')
 
     # resample to a daily composite
     goes_nc = goes_nc.drop(['quality_level'])
     goes_nc = goes_nc.resample(time='1D').mean('time')
     
-    newtimestamp = (newtimestamp - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    newtimestamp = (newtimestamp - np.datetime64('1970-01-01T00:00:00')) / np.timedelta64(1, 's')
     goes_nc.time.values = np.array([newtimestamp], dtype='float64')
     goes_nc.time.attrs['units'] = 'seconds since 1970-01-01 00:00:00'
     
     #landmask.to_netcdf(path=outpath2 + 'landmask_roffs_' +  'area' + str(a) + '_' + str(goes_nc.time.values[0])[0:10] + '_' + str(goes_nc.time.values[-1])[0:10] + '.nc', format='NETCDF3_CLASSIC')
     goes_nc.to_netcdf(path=outpath2 + '/' + str(datelist[d].year) + '/GOES16_SST_dailycomposite_' + str(datelist[d].year) + str("{0:0=3d}".format(
-        datelist[d].dayofyear)) + '_' + str("{0:0=2d}".format(datelist[d].month)) + str("{0:0=2d}".format(datelist[d].day)) + '.nc', format='NETCDF3_CLASSIC')
+        datelist[d].dayofyear)) + '_' + str("{0:0=2d}".format(datelist[d].month)) + str("{0:0=2d}".format(datelist[d].day)) + '.nc', mode='w',format='NETCDF4')
 
     
