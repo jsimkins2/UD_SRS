244a38fd83a3ce6d66ad9fa2248031a5aa68e37b
Default Seadas User <seadas-user@localhost>
2020-04-15 11:47:45 -0400

    adding del nc attrs

--- sst_goes16/jpl_goes_extraction.py
+++ sst_goes16/jpl_goes_extraction.py
@@ -169,9 +169,9 @@ goes_nc.time.attrs['units'] = 'seconds since 1970-01-01 00:00:00'
 
 # have to add the following line becuase of a weird xarray netcdf4 error when writing the xarray to netcdf
 #del goes_nc.attrs['_NCProperties']
-
+tempNC = goes_nc
 outpath = "/data/GOES/GOES-R/daily_composite/"
-goes_nc.to_netcdf(path=outpath + '/' + str(today[0].year) + '/GOES16_SST_dailycomposite_' + str(today[0].year) + str("{0:0=3d}".format(
+tempNC.to_netcdf(path=outpath + '/' + str(today[0].year) + '/GOES16_SST_dailycomposite_' + str(today[0].year) + str("{0:0=3d}".format(
     today[0].dayofyear)) + '_' + str("{0:0=2d}".format(today[0].month)) + str("{0:0=2d}".format(today[0].day)) + '.nc', mode='w',format='NETCDF4')
 
 
