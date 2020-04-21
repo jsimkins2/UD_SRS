0a93f894715805fe1e635abfbea3e89e31b36105
Default Seadas User <seadas-user@localhost>
2020-04-21 09:45:33 -0400

    reprocessing the last 4 days for the 24h ncep 4 file on each run. We do this because the files get revised for a few days after they drop

--- nexrad/ncep_stageIV/download_24h_ncepStageIV.R
+++ nexrad/ncep_stageIV/download_24h_ncepStageIV.R
@@ -12,31 +12,29 @@ stageUrls = list()
 for (d in seq(utc14, utcnow, by="hour")){
   dateTime = as.POSIXct.numeric(d,tz='UTC',origin='1970-01-01 00:00:00')
   temFile = paste0("ST4.", year(dateTime),sprintf("%02i", month(dateTime)), sprintf("%02i", day(dateTime)),'12.24h.gz')
-  if (!file.exists(paste0(datadir,'notNetcdf/', substr(temFile, 0, nchar(temFile)-3)))){
-    system(paste0("/usr/bin/wget -P /home/james/ncep_stageIV/24h/zipped/ https://nomads.ncep.noaa.gov/pub/data/nccf/com/pcpanl/prod/pcpanl.",year(dateTime),sprintf("%02i", month(dateTime)), sprintf("%02i", day(dateTime)),"/",temFile))
-    system(paste0("/usr/bin/gunzip ",datadir,'zipped/', temFile))
-    temFile=substr(temFile, 0, nchar(temFile)-3)
-    system(paste0("/usr/bin/mv ",datadir,'zipped/',temFile, " ", datadir,'notNetcdf/'))
-    system(paste0("/usr/local/bin/gdal_translate ",datadir,"notNetcdf/",temFile, " ",datadir,'unprojected/', temFile, ".nc -of netcdf"))
-    system(paste0("/usr/local/bin/gdalwarp ",datadir,'unprojected/', temFile,".nc /data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc -of netcdf -t_srs epsg:4326"))
     
-    f=paste0(temFile,".nc")
-    timeVal = as.POSIXct(ymd_h(substr(f,5,14)))
-    loc = nc_open(paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"),write = TRUE)
-    prec = ncvar_get(nc=loc, varid="Band1")
-    timedim = ncdim_def("time", "seconds since 1970-01-01",as.integer(timeVal))
-    londim = loc$dim$lon
-    latdim = loc$dim$lat
-    
-    prec_def = ncvar_def(name="Precipitation_Flux", units="mm/hr", missval=loc$var$Band1$missval, longname = "Quantitative Precipitation Estimates", dim=list(londim,latdim,timedim))
-    ncdf4::ncvar_add(nc=loc, v=prec_def)
-    nc_close(loc)
-    
-    loc = nc_open(paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"),write = TRUE)
-    ncvar_put(nc=loc,varid="Precipitation_Flux", vals=prec)
-    nc_close(loc)
-    
-    system(paste0("/usr/bin/ncks -x -v Band1 ",paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc")," ",paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"), " -O"))
-    
-  }
+  system(paste0("/usr/bin/wget -P /home/james/ncep_stageIV/24h/zipped/ https://nomads.ncep.noaa.gov/pub/data/nccf/com/pcpanl/prod/pcpanl.",year(dateTime),sprintf("%02i", month(dateTime)), sprintf("%02i", day(dateTime)),"/",temFile))
+  system(paste0("/usr/bin/gunzip ",datadir,'zipped/', temFile))
+  temFile=substr(temFile, 0, nchar(temFile)-3)
+  system(paste0("/usr/bin/mv ",datadir,'zipped/',temFile, " ", datadir,'notNetcdf/'))
+  system(paste0("/usr/local/bin/gdal_translate ",datadir,"notNetcdf/",temFile, " ",datadir,'unprojected/', temFile, ".nc -of netcdf"))
+  system(paste0("/usr/local/bin/gdalwarp ",datadir,'unprojected/', temFile,".nc /data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc -of netcdf -t_srs epsg:4326"))
+  
+  f=paste0(temFile,".nc")
+  timeVal = as.POSIXct(ymd_h(substr(f,5,14)))
+  loc = nc_open(paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"),write = TRUE)
+  prec = ncvar_get(nc=loc, varid="Band1")
+  timedim = ncdim_def("time", "seconds since 1970-01-01",as.integer(timeVal))
+  londim = loc$dim$lon
+  latdim = loc$dim$lat
+  
+  prec_def = ncvar_def(name="Precipitation_Flux", units="mm/hr", missval=loc$var$Band1$missval, longname = "Quantitative Precipitation Estimates", dim=list(londim,latdim,timedim))
+  ncdf4::ncvar_add(nc=loc, v=prec_def)
+  nc_close(loc)
+  
+  loc = nc_open(paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"),write = TRUE)
+  ncvar_put(nc=loc,varid="Precipitation_Flux", vals=prec)
+  nc_close(loc)
+  
+  system(paste0("/usr/bin/ncks -x -v Band1 ",paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc")," ",paste0("/data/ncep_stageIV/quality/",year(dateTime),"/",temFile, "r.nc"), " -O"))
 }
