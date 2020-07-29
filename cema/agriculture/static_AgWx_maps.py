39292170fcec9fc9e7c1307d574c1da96d0318b8
james <jamessimkins5@gmail.com>
2020-07-29 09:36:35 -0400

    saving static updates where i added in teh year to date cumsum totals for summed datasets

--- cema/agriculture/static_AgWx_maps.py
+++ cema/agriculture/static_AgWx_maps.py
@@ -1,4 +1,4 @@
-# deos geopandas to be run at home 
+# static ag weather maps hosted on /var/www/html/imagery/AgWx/
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,6 +26,8 @@ import time
 import matplotlib.image as image
 from datetime import datetime, timedelta, date
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 import rioxarray
 
 # declare paths
@@ -187,7 +189,7 @@ nowtime = datetime.utcnow()
 ytd = pd.to_datetime(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d")) -  pd.to_datetime(nowtime)
 daysback_dict = dict(zip(['YTD', '3 Months', '1 Month', '1 Week', '1 Day'], [np.int(np.abs(ytd.days)), 90, 30, 7, 1]))
 datasets = list(sum_dict.keys()) + list(mean_dict.keys()) + list(max_dict.keys()) + list(min_dict.keys())
-'''
+
 nowdate=datetime.utcnow()
 for var in datasets:
     print(var)
@@ -382,7 +384,7 @@ for var in datasets:
 
         plt.close()
         
-'''
+
 # now create the departure maps 
 # load in the datasets
 
@@ -486,7 +488,7 @@ for var in datasets:
 # list of dataframes
 countydf = ['chester_agwx.nc', 'ncc_agwx.nc', 'kentc_agwx.nc', 'sussex_agwx.nc']
 doydf = ['chester_agwx_climatology.nc', 'ncc_agwx_climatology.nc', 'kent_agwx_climatology.nc', 'sussex_agwx_climatology.nc']
-
+co_names = ['Chester', 'New Castle', 'Kent', 'Sussex']
 nowtime = datetime.utcnow()
 ytd = pd.to_datetime(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d")) -  pd.to_datetime(nowtime)
 daysback_dict = dict(zip(['YTD'], [np.int(np.abs(ytd.days))]))#dict(zip(['YTD', '3 Months', '1 Month', '1 Week', '1 Day'], [np.int(np.abs(ytd.days)), 90, 30, 7, 1]))
@@ -498,31 +500,150 @@ for db in daysback_dict.keys():
         df = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['dailyprecip']
         time_recent = pd.to_datetime(df.time.values[-1])
         df = df.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        df.values = df.values * (0.0393701)
         cf = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['dailyprecip']
         cf = cf.isel(dayofyear=slice(0, 366))
+        cf.values = cf.values * (0.0393701)
         ncep = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['NCEPstageIVPrecip']
-        ncep.values = ncep.values
         ncep = ncep.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        ncep.values = ncep.values * (0.0393701)
         ncepClim = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['NCEPstageIVPrecip']
-        ncepClim.values = ncepClim.values
+        ncepClim.values = ncepClim.values * (0.0393701)
         ncepClim = ncepClim.isel(dayofyear=slice(0, 366))
-        
-        xdate = range(datetime.strptime("2020-01-01", "%Y-%m-%d"), datetime.strptime("2020-12-31", "%Y-%m-%d"))
-        datelist = pd.date_range("2020-01-01", "2020-12-31").tolist()
-        fig, ax = plt.subplots()
-        ax.plot(ncepClim.dayofyear.values, np.cumsum(ncepClim.values))
-        ax.set_xlim([datetime.date(2020, 1, 1), datetime.date(2020, 12, 31)])
-        
         #Set X range here:
         left = date(2020, 1, 1)  #Makes it easy to quickly change the range
         right = date(2020, 12, 31)
+        datelist = pd.date_range("2020-01-01", "2020-12-31").tolist()
+
+
         fig = plt.figure(figsize=(12,8))
-        #Create scatter plot of Postitive Cases
-        plt.plot(datelist, np.cumsum(ncepClim.values), c="blue",  label="Total Positive Tested")
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b')) #Allows me to format the date into months & days
+        # Create subplot of 
+        plt.subplot(2, 1, 1)
+        plt.plot(datelist, np.cumsum(cf.values), linestyle='dashed', c="lightseagreen",  label="Climatology")
+        plt.plot(df.time.values, np.cumsum(df.values), c="darkgreen",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
         plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
         plt.gca().set_xbound(left, right)  #changes the range of the x-axis
-        
-        plt.show()
-            
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - DEOS Precipitation')
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Accumulated Inches (YTD)', labelpad = 2)
+        plt.legend()
+        plt.grid()
+        fig.tight_layout()
+        #Create subplot of ncep
+        plt.subplot(2,1,2)
+        plt.plot(datelist, np.cumsum(ncepClim.values), linestyle='dashed', c="deepskyblue",  label="Climatology")
+        plt.plot(ncep.time.values, np.cumsum(ncep.values), c="blue",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
+        plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
+        plt.gca().set_xbound(left, right)  #changes the range of the x-axis
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - NCEP Stage IV Precipitation')
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Accumulated Inches (YTD)', labelpad = 2)
+        plt.legend()
+        plt.grid()
+        im1 = image.imread(shapePaths + "deos_logo.png")
+        plt.figimage(im1, 1450, 75 ,zorder=30, alpha=1)
+        fig.tight_layout()
+        plt.savefig("/var/www/html/imagery/AgWx/county/" + co_names[co] + "_YTD_precipitation.png",bbox_inches='tight',pad_inches = 0.1,dpi=my_dpi*1.3)
+
+# HDD and CDD 
+nowdate=datetime.utcnow()
+for db in daysback_dict.keys():
+    for co in range(0,len(countydf)):
+        df = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['HDD']
+        time_recent = pd.to_datetime(df.time.values[-1])
+        df = df.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        cf = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['HDD']
+        cf = cf.isel(dayofyear=slice(0, 366))
+        ncep = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['CDD']
+        ncep = ncep.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        ncepClim = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['CDD']
+        ncepClim = ncepClim.isel(dayofyear=slice(0, 366))
+        #Set X range here:
+        left = date(2020, 1, 1)  #Makes it easy to quickly change the range
+        right = date(2020, 12, 31)
+        datelist = pd.date_range("2020-01-01", "2020-12-31").tolist()
+
+
+        fig = plt.figure(figsize=(12,8))
+        # Create subplot of 
+        plt.subplot(2, 1, 1)
+        plt.plot(datelist, np.cumsum(cf.values), linestyle='dashed', c="coral",  label="Climatology")
+        plt.plot(df.time.values, np.cumsum(df.values), c="maroon",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
+        plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
+        plt.gca().set_xbound(left, right)  #changes the range of the x-axis
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - DEOS Heating Degree Days')
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Total Days', labelpad = 2)
+        plt.legend()
+        plt.grid()
+        fig.tight_layout()
+        #Create subplot of ncep
+        plt.subplot(2,1,2)
+        plt.plot(datelist, np.cumsum(ncepClim.values), linestyle='dashed', c="deepskyblue",  label="Climatology")
+        plt.plot(ncep.time.values, np.cumsum(ncep.values), c="blue",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
+        plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
+        plt.gca().set_xbound(left, right)  #changes the range of the x-axis
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - DEOS Cooling Degree Days')
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Total Days', labelpad = 1)
+        plt.legend()
+        plt.grid()
+        im1 = image.imread(shapePaths + "deos_logo.png")
+        plt.figimage(im1, 1450, 75 ,zorder=30, alpha=1)
+        fig.tight_layout()
+        plt.savefig("/var/www/html/imagery/AgWx/county/" + co_names[co] + "_YTD_HDD_CDD.png",bbox_inches='tight',pad_inches = 0.1,dpi=my_dpi*1.3)
+
+# GDD and Energy Density
+nowdate=datetime.utcnow()
+for db in daysback_dict.keys():
+    for co in range(0,len(countydf)):
+        df = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['GDD']
+        time_recent = pd.to_datetime(df.time.values[-1])
+        df = df.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        cf = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['GDD']
+        cf = cf.isel(dayofyear=slice(0, 366))
+        ncep = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + countydf[co])['energyDens']
+        ncep = ncep.sel(time=slice(datetime.strptime(str(str(nowtime.year) + '-01-01'), "%Y-%m-%d"), time_recent))
+        ncepClim = xr.open_dataset('http://thredds.demac.udel.edu/thredds/dodsC/' + doydf[co])['energyDens']
+        ncepClim = ncepClim.isel(dayofyear=slice(0, 366))
+        #Set X range here:
+        left = date(2020, 1, 1)  #Makes it easy to quickly change the range
+        right = date(2020, 12, 31)
+        datelist = pd.date_range("2020-01-01", "2020-12-31").tolist()
+
+
+        fig = plt.figure(figsize=(12,8))
+        # Create subplot of 
+        plt.subplot(2, 1, 1)
+        plt.plot(datelist, np.cumsum(cf.values), linestyle='dashed', c="lightseagreen",  label="Climatology")
+        plt.plot(df.time.values, np.cumsum(df.values), c="darkgreen",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
+        plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
+        plt.gca().set_xbound(left, right)  #changes the range of the x-axis
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - DEOS Growing Degree Days', pad=5)
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Total Days', labelpad = 4)
+        plt.legend()
+        plt.grid()
+        fig.tight_layout()
+        #Create subplot of ncep
+        plt.subplot(2,1,2)
+        plt.plot(datelist, np.cumsum(ncepClim.values), linestyle='dashed', c="deepskyblue",  label="Climatology")
+        plt.plot(ncep.time.values, np.cumsum(ncep.values), c="blue",  label="2020 Year to Date")
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-01')) #Allows me to format the date into months & days
+        plt.gca().xaxis.set_tick_params(rotation = 0)  #puts the x-axis labels on an angle
+        plt.gca().set_xbound(left, right)  #changes the range of the x-axis
+        plt.title(str(nowtime.year) + ' ' + co_names[co] + ' County - DEOS Energy Density', pad=5)
+        plt.xlabel('Date', labelpad = 5)
+        plt.ylabel('Total Energy', labelpad = 2)
+        plt.legend()
+        plt.grid()
+        im1 = image.imread(shapePaths + "deos_logo.png")
+        plt.figimage(im1, 1450, 75 ,zorder=30, alpha=1)
+        fig.tight_layout()
+        plt.savefig("/var/www/html/imagery/AgWx/county/" + co_names[co] + "_YTD_GDD_Energy.png",bbox_inches='tight',pad_inches = 0.1,dpi=my_dpi*1.3)
 
