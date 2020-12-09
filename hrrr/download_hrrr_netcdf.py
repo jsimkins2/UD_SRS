f2e8d3dc3606a8b1231a8dc30e6cab06931f281f
james <jamessimkins5@gmail.com>
2020-12-09 11:17:37 -0500

    updated try except statment for hrrr grib2 to netcdf file

--- hrrr/download_hrrr_netcdf.py
+++ hrrr/download_hrrr_netcdf.py
@@ -30,26 +30,19 @@ if os.path.isfile(datadir + dataset_name[:-5] + 'nc') == False:
     tempiso = ds.metpy.parse_cf('Temperature_isobaric')
     hproj = tempiso.metpy.cartopy_crs
     hproj = Proj(hproj.proj4_init)
-    
-    
     # Write the netcdf file
-    
     f = Dataset(datadir + dataset_name[:-5] + 'nc','w', format='NETCDF4') #'w' stands for write
     # dimensions
     f.createDimension('x', tempiso['x'].size)
     f.createDimension('y', tempiso['y'].size)
     f.createDimension('time', 1)
-    
     # variables
-    
     x = f.createVariable('x', 'f4', 'x')
     x.standard_name = tempiso['x'].standard_name
     x.units = tempiso['x'].units
-    
     y = f.createVariable('y', 'f4', 'y')
     y.standard_name = tempiso['y'].standard_name
     y.units = tempiso['y'].units
-    
     time = f.createVariable('time', 'f8', 'time')
     try:
         time.standard_name = tempiso['time'][1].standard_name
@@ -60,31 +53,25 @@ if os.path.isfile(datadir + dataset_name[:-5] + 'nc') == False:
             time.long_name = tempiso['time1'][1].long_name
         except:
             pass
-    
     proj = f.createVariable('projection', 'f4')
     proj.crs = 'lambert_conformal_conic'
     proj.srs = hproj.srs
-    
     tsurf = f.createVariable('temperature_surface', 'f4', ('time', 'y', 'x'))
     tsurf.long_name = 'Temperature @ Surface'
     tsurf.standard_name = 'temperature_surface'
     tsurf.units = 'Kelvin'
-    
     t925 = f.createVariable('temperature_925', 'f4', ('time', 'y', 'x'))
     t925.long_name = 'Temperature @ 925mb'
     t925.standard_name = 'temperature_925'
     t925.units = 'Kelvin'
-    
     t850 = f.createVariable('temperature_850', 'f4', ('time', 'y', 'x'))
     t850.long_name = 'Temperature @ 850mb'
     t850.standard_name = 'temperature_850'
     t850.units = 'Kelvin'
-    
     ht500 = f.createVariable('height_500', 'f4', ('time', 'y', 'x'))
     ht500.long_name = 'Geopotential_height_isobaric @ 500mb'
     ht500.standard_name = 'height_500'
     ht500.units = 'gpm'
-    
     ht1000 = f.createVariable('height_1000', 'f4', ('time', 'y', 'x'))
     ht1000.long_name = 'Geopotential_height_isobaric @ 1000mb'
     ht1000.standard_name = 'height_1000'
@@ -93,7 +80,11 @@ if os.path.isfile(datadir + dataset_name[:-5] + 'nc') == False:
     # fill the data
     x[:] = tempiso['x'].values
     y[:] = tempiso['y'].values
-    time[:] = tempiso['time'][1].values
+    try:
+        time[:] = tempiso['time'][1].values
+    except:
+        try:
+            time[:] = tempiso['time1'][1].values
     tsurf[:]= ds.metpy.parse_cf('Temperature_height_above_ground')[1][0].values
     t925[:]=tempiso[1][3].values
     t850[:]=tempiso[1][2].values
