7b9d068bdefdc8fced15e9beb0524a6b923207f0
james <jamessimkins5@gmail.com>
2021-01-18 12:28:47 -0500

    updated backfill code

--- sst_goes16/noaa_goes16_sst/backfill/backfill.py
+++ sst_goes16/noaa_goes16_sst/backfill/backfill.py
@@ -32,7 +32,7 @@ dataset_name = sorted(cat.datasets.keys())[-1]
 dataset = cat.datasets[dataset_name]
 
 ds = dataset.remote_access(service='OPENDAP')
-d = ds
+orig_d = ds
 
 def nearest(items, pivot):
     return min(items, key=lambda x: abs(x - pivot))
@@ -49,73 +49,178 @@ def JulianDate_to_MMDDYYY(y,jd):
 datadir = "/data/GOES/GOES-R/backfill/2018/"
 sst_data = sorted([f for f in listdir(datadir) if isfile(join(datadir, f))])
 
-ldatetime = []
-
 
 for fname in sst_data:
-    ds = xr.open_dataset(datadir + fname)
-    dat = ds.metpy.parse_cf('SST')
-    proj = dat.metpy.cartopy_crs
-    dat_dqf = ds.metpy.parse_cf('DQF')
-
-    dat = dat.where(dat > -1)
-    dat.values[np.isnan(dat.values)] = -999
-    # Now grab band 15
     ftime = fname.split('_')[3][1:-3]
-
     gdatetime=datetime.strptime(ftime, '%Y%j%H%M')
+    dataset = fname
+    # rename in case they added an M6 instead of M3
+    if str(dataset).split('_')[1] != 'ABI-L2-SSTF-M3':
+        dataset_name = str(dataset).split('_')[0] + '_ABI-L2-SSTF-M3_G16' + str(dataset).split('G16')[1]
+    else:
+        dataset_name = str(dataset)
+    if os.path.isfile("/data/GOES/GOES-R/sst/" + str(gdatetime.year) + "/" + str(dataset_name)) == False:
+        if os.path.isfile("/data/GOES/GOES-R/suspect/"+ str(dataset_name)) == False:
+            print("at the top" + dataset_name)
+            ds = xr.open_dataset(datadir + fname)
+            dat = ds.metpy.parse_cf('SST')
+            proj = dat.metpy.cartopy_crs
+            dat_dqf = ds.metpy.parse_cf('DQF')
+        
+            dat = dat.where(dat > -1)
+            dat.values[np.isnan(dat.values)] = -999
+        
+            f = Dataset("/home/sat_ops/goesR/data/noaa_sst/backfill/raw/" + str(nowdate.year) + "/" + str(fname),'w', format='NETCDF4') #'w' stands for write
+            # dimensions
+            f.createDimension('x', dat['x'].size)
+            f.createDimension('y', dat['y'].size)
+            f.createDimension('time', 1)
+        
+            # variables
+        
+            x = f.createVariable('x', 'f4', 'x')
+            x.standard_name = dat['x'].standard_name
+            x.units = dat['x'].units
+        
+            y = f.createVariable('y', 'f4', 'y')
+            y.standard_name = dat['y'].standard_name
+            y.units = dat['y'].units
+        
+            time = f.createVariable('time', 'f8', 'time')
+            time.standard_name = dat['t'].standard_name
+            time.long_name = dat['t'].long_name
+        
+            proj = f.createVariable('goes_imager_projection', 'f4')
+            proj.grid_mapping_name = orig_d.variables['goes_imager_projection'].grid_mapping_name
+            proj.perspective_point_height = orig_d.variables['goes_imager_projection'].perspective_point_height
+            proj.semi_major_axis = orig_d.variables['goes_imager_projection'].semi_major_axis
+            proj.semi_minor_axis = orig_d.variables['goes_imager_projection'].semi_minor_axis
+            proj.inverse_flattening = orig_d.variables['goes_imager_projection'].inverse_flattening
+            proj.latitude_of_projection_origin = orig_d.variables['goes_imager_projection'].latitude_of_projection_origin
+            proj.longitude_of_projection_origin = orig_d.variables['goes_imager_projection'].longitude_of_projection_origin
+            proj.sweep_angle_axis = orig_d.variables['goes_imager_projection'].sweep_angle_axis
+        
+            sst = f.createVariable('SST', 'f4', ('time', 'y', 'x'))
+            sst.long_name = orig_d.variables['SST'].long_name
+            sst.standard_name = orig_d.variables['SST'].standard_name
+            sst.units = orig_d.variables['SST'].units
+            sst.resolution = orig_d.variables['SST'].resolution
+            sst.missing_value = orig_d.variables['SST']._FillValue
+            sst.grid_mapping = 'goes_imager_projection'
+        
+            dqf = f.createVariable('DQF', 'f4', ('time', 'y', 'x'))
+            dqf.long_name = orig_d.variables['DQF'].long_name
+            dqf.standard_name = orig_d.variables['DQF'].standard_name
+            dqf.units = orig_d.variables['DQF'].units
+            dqf.flag_values = orig_d.variables['DQF'].flag_values
+            dqf.flag_meanings = orig_d.variables['DQF'].flag_meanings
+            dqf.grid_mapping = 'goes_imager_projection'
+        
+            # data
+            x[:] = dat['x'].values
+            y[:] = dat['y'].values
+            sst[:]= dat.values
+            dqf[:]=dat_dqf
+            time[:] = dat['t'].values
+            f.close()
+            
+            print("completed initial saving of raw file")
+            reproject = "Rscript /home/sat_ops/goesR/data/noaa_sst/backfill/reproject_sst.R " + str(fname)
+            os.system(reproject)
+            
+            print("ALL DONE REPROJECTING")
+            nowdate = datetime.utcnow()
+            
+        
+            d = Dataset("/home/sat_ops/goesR/data/noaa_sst/backfill/raw/" + str(nowdate.year) + "/" + str(dataset_name))
+            ds = NetCDF4DataStore(d)
+            ds = xr.open_dataset(ds)
+            dat = ds.metpy.parse_cf('SST')
+            proj = dat.metpy.cartopy_crs
+            print("/home/sat_ops/goesR/data/noaa_sst/backfill/rast_sst/" + str(nowdate.year) + "/SST" + str(dataset))
+            dat_dqf  = xr.open_dataset("/home/sat_ops/goesR/data/noaa_sst/backfill/rast_dqf/" + str(nowdate.year) + "/DQF" + str(dataset))
+            new = xr.open_dataset("/home/sat_ops/goesR/data/noaa_sst/backfill/rast_sst/" + str(nowdate.year) + "/SST" + str(dataset))
+    
+            new = new.where((new['latitude']>16) & (new['latitude']<52) & (new['longitude']>-100) & (new['longitude']<-50), drop=True)
+            dat_dqf = dat_dqf.where((dat_dqf['latitude']>16) & (dat_dqf['latitude']<52) & (dat_dqf['longitude']>-100) & (dat_dqf['longitude']<-50), drop=True)
+            # now write it all to netcdf!
+            print("WRITING FINAL FILE TO NETCDF")
+            f = Dataset("/data/GOES/GOES-R/sst/" + str(gdatetime.year) + "/" + str(dataset_name),'w', format='NETCDF4') #'w' stands for write
+            # dimensions
+            f.createDimension('longitude', new['longitude'].size)
+            f.createDimension('latitude', new['latitude'].size)
+            f.createDimension('time', 1)
+    
+            # variables
+            longitude = f.createVariable('longitude', 'f4', 'longitude')
+            longitude.standard_name = new['longitude'].long_name
+            longitude.units = new['longitude'].units
+    
+            latitude = f.createVariable('latitude', 'f4', 'latitude')
+            latitude.standard_name = new['latitude'].long_name
+            latitude.units = new['latitude'].units
+    
+            time = f.createVariable('time', 'f8', 'time')
+            time.standard_name = "time"
+            time.long_name = "EPOCH Time"
+            time.units = "seconds since 1970-01-01T00:00:00Z"
+    
+            proj = f.createVariable('projection', 'f4')
+            proj.proj4_string = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+            proj.epsg_code = 4326
+    
+            sst = f.createVariable('SST', 'f4', ('time', 'latitude', 'longitude'))
+            sst.long_name = d.variables['SST'].long_name
+            sst.standard_name = d.variables['SST'].standard_name
+            sst.units = d.variables['SST'].units
+            sst.resolution = d.variables['SST'].resolution
+            sst.missing_value = -999.0
+    
+            dqf = f.createVariable('DQF', 'f4', ('time', 'latitude', 'longitude'))
+            dqf.long_name = d.variables['DQF'].long_name
+            dqf.standard_name = d.variables['DQF'].standard_name
+            dqf.units = d.variables['DQF'].units
+            dqf.flag_values = d.variables['DQF'].flag_values
+            dqf.flag_meanings = d.variables['DQF'].flag_meanings
+    
+    
+            longitude[:] = new['longitude'].values
+            sst[:]= new['sst'].values
+            dqf[:]=dat_dqf['dqf'].values
+            time[:] = (dat['time'].values.astype('uint64') / 1e9).astype('uint32')
+    
+            # metadata
+            f.creator_name = "James Simkins"
+            f.creator_email = "simkins@udel.edu"
+            f.institution = "University of Delaware Ocean Exploration, Remote Sensing and Biogeography Group (ORB)"
+            f.url = "http://orb.ceoe.udel.edu/"
+            f.source = "satellite observation NASA MODIS-Aqua instrument"
+            f.groundstation = "University of Delaware, Newark, Center for Remote Sensing"
+            f.software = 0.0
+            f.inputMET1 = 0.0
+            f.inputOZONE1 = 0.0
+            f.inputCalibrationFile = 0.0
+            f.product_list = "SST, DQF"
+            f.summary = "GOES16 SST product, reprojected to EPSG:4326."
+    
+            f.close()
+            # add netcdf file compression.  -L 5 means level 5 compression, -O means overwrite
+            fname2 = "/data/GOES/GOES-R/sst/" + str(gdatetime.year) + "/" + str(dataset_name)
+            temp_str = "ncks " + fname2 + " " + fname2 + " -L 5 -O"
+            os.system(temp_str)
+            
+            print("flipping lat")
+            flip_lat = "Rscript /home/sat_ops/goesR/data/noaa_sst/backfill/flip_lat_sst.R " + fname2
+            os.system(flip_lat)
+            os_dat1  = "rm /home/sat_ops/goesR/data/noaa_sst/backfill/rast_dqf/" + str(nowdate.year) + "/DQF" + str(dataset)
+            os.system(os_dat1)
+            os_dat2 = "rm /home/sat_ops/goesR/data/noaa_sst/backfill/rast_sst/" + str(nowdate.year) + "/SST" + str(dataset)
+            print(os_dat2)
+            os.system(os_dat2)
+            os_dat3 = "rm /home/sat_ops/goesR/data/noaa_sst/backfill/raw/" + str(nowdate.year) + "/" + str(fname)
+            os.system(os_dat3)
 
-    f = Dataset("/home/sat_ops/goesR/data/noaa_sst/backfill/raw/" + str(nowdate.year) + "/" + str(fname),'w', format='NETCDF4') #'w' stands for write
-    # dimensions
-    f.createDimension('x', dat['x'].size)
-    f.createDimension('y', dat['y'].size)
-    f.createDimension('time', 1)
 
-    # variables
 
-    x = f.createVariable('x', 'f4', 'x')
-    x.standard_name = dat['x'].standard_name
-    x.units = dat['x'].units
 
-    y = f.createVariable('y', 'f4', 'y')
-    y.standard_name = dat['y'].standard_name
-    y.units = dat['y'].units
-
-    time = f.createVariable('time', 'f8', 'time')
-    time.standard_name = dat['t'].standard_name
-    time.long_name = dat['t'].long_name
-
-    proj = f.createVariable('goes_imager_projection', 'f4')
-    proj.grid_mapping_name = d.variables['goes_imager_projection'].grid_mapping_name
-    proj.perspective_point_height = d.variables['goes_imager_projection'].perspective_point_height
-    proj.semi_major_axis = d.variables['goes_imager_projection'].semi_major_axis
-    proj.semi_minor_axis = d.variables['goes_imager_projection'].semi_minor_axis
-    proj.inverse_flattening = d.variables['goes_imager_projection'].inverse_flattening
-    proj.latitude_of_projection_origin = d.variables['goes_imager_projection'].latitude_of_projection_origin
-    proj.longitude_of_projection_origin = d.variables['goes_imager_projection'].longitude_of_projection_origin
-    proj.sweep_angle_axis = d.variables['goes_imager_projection'].sweep_angle_axis
-
-    sst = f.createVariable('SST', 'f4', ('time', 'y', 'x'))
-    sst.long_name = d.variables['SST'].long_name
-    sst.standard_name = d.variables['SST'].standard_name
-    sst.units = d.variables['SST'].units
-    sst.resolution = d.variables['SST'].resolution
-    sst.missing_value = d.variables['SST']._FillValue
-    sst.grid_mapping = 'goes_imager_projection'
-
-    dqf = f.createVariable('DQF', 'f4', ('time', 'y', 'x'))
-    dqf.long_name = d.variables['DQF'].long_name
-    dqf.standard_name = d.variables['DQF'].standard_name
-    dqf.units = d.variables['DQF'].units
-    dqf.flag_values = d.variables['DQF'].flag_values
-    dqf.flag_meanings = d.variables['DQF'].flag_meanings
-    dqf.grid_mapping = 'goes_imager_projection'
-
-    # data
-    x[:] = dat['x'].values
-    y[:] = dat['y'].values
-    sst[:]= dat.values
-    dqf[:]=dat_dqf
-    time[:] = dat['t'].values
-    f.close()
 
--- sst_goes16/noaa_goes16_sst/backfill/reproject_sst.R
+++ sst_goes16/noaa_goes16_sst/backfill/reproject_sst.R
@@ -3,45 +3,38 @@ library(raster)
 library(ncdf4)
 
 options(warn=-1)
-
-endyr = lubridate::year(Sys.Date())
-yearseq = seq(2018,endyr)
+args = commandArgs(trailingOnly=TRUE)
+fname = args[1]
+yr = lubridate::year(Sys.Date())
 datadir = "/home/sat_ops/goesR/data/noaa_sst/backfill/"
-for (y in yearseq){
-  fnamelist = list.files(paste0(datadir,"raw/", y, "/"))
-  for (i in fnamelist){
-    if (!file.exists(paste0(datadir, "rast_sst/", y,"/SST", i))){
-      print(i)
-      goesfile <- nc_open(paste0(datadir,"raw/", y, "/",i))
-      time.val = ncvar_get(goesfile, "time")
-      
-      sst = ncvar_get(goesfile, "SST")
-      sst_raster = raster(sst)
-      sst_raster = t(sst_raster)
-      extent(sst_raster)=extent(-5433893, 5433893, -5433893, 5433893)
-      crs(sst_raster) = "+units=m +lon_0=-75.0 +h=35786023.0 +sweep=x +proj=geos"
-      
-      # we use epsg:4326 or ccrs.Geodetic
-      newproj <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-      projected_sst = projectRaster(sst_raster, crs = newproj)
-      writeRaster(projected_sst, filename=paste0(datadir, "rast_sst/", y, "/SST", i), format="CDF", overwrite=TRUE,
-                  varname="sst", varunit="Kelvin", longname="sea surface temperature",
-                  xname="longitude", yname="latitude")
-      
-      dqf = ncvar_get(goesfile, "DQF")
-      dqf_raster = raster(dqf)
-      dqf_raster = t(dqf_raster)
-      extent(dqf_raster)=extent(-5433893, 5433893, -5433893, 5433893)
-      crs(dqf_raster) = "+units=m +lon_0=-75.0 +h=35786023.0 +sweep=x +proj=geos"
-      print("writing dqf file")
-      projected_dqf = projectRaster(dqf_raster, crs = newproj)
-      writeRaster(projected_dqf, filename=paste0(datadir, "rast_dqf/", y, "/DQF", i), format="CDF", overwrite=TRUE,
-                  varname="dqf", varunit="Kelvin", longname="data quality flags",
-                  xname="longitude", yname="latitude")
-      
-      nc_close(goesfile)
-    }
-  }
-}
+
+goesfile <- nc_open(paste0(datadir,"raw/", yr, "/",fname))
+time.val = ncvar_get(goesfile, "time")
+
+sst = ncvar_get(goesfile, "SST")
+sst_raster = raster(sst)
+sst_raster = t(sst_raster)
+extent(sst_raster)=extent(-5433893, 5433893, -5433893, 5433893)
+crs(sst_raster) = "+units=m +lon_0=-75.0 +h=35786023.0 +sweep=x +proj=geos"
+
+# we use epsg:4326 or ccrs.Geodetic
+newproj <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+projected_sst = projectRaster(sst_raster, crs = newproj)
+writeRaster(projected_sst, filename=paste0(datadir, "rast_sst/", yr, "/SST", fname), format="CDF", overwrite=TRUE,
+          varname="sst", varunit="Kelvin", longname="sea surface temperature",
+          xname="longitude", yname="latitude")
+
+dqf = ncvar_get(goesfile, "DQF")
+dqf_raster = raster(dqf)
+dqf_raster = t(dqf_raster)
+extent(dqf_raster)=extent(-5433893, 5433893, -5433893, 5433893)
+crs(dqf_raster) = "+units=m +lon_0=-75.0 +h=35786023.0 +sweep=x +proj=geos"
+print("writing dqf file")
+projected_dqf = projectRaster(dqf_raster, crs = newproj)
+writeRaster(projected_dqf, filename=paste0(datadir, "rast_dqf/", yr, "/DQF", fname), format="CDF", overwrite=TRUE,
+          varname="dqf", varunit="Kelvin", longname="data quality flags",
+          xname="longitude", yname="latitude")
+
+nc_close(goesfile)
 
 options(warn=0)
